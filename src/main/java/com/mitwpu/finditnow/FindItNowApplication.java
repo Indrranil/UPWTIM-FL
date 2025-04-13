ca251ac268d0450002f4b65e8976d189c169e6ea
Indrranil Pawar <indrranil7@gmail.com>
2025-04-13 15:34:49 +0530

    Add Spring Boot main class

---
+++ src/main/java/com/mitwpu/finditnow/FindItNowApplication.java
@@ -0,0 +1,12 @@
+package com.mitwpu.finditnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindItNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindItNowApplication.class, args);
+    }
+}
